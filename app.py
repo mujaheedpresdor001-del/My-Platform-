a05bdfa9caaf9165e855fae9ff65594b8d76e53d
mujaheedpresdor001-del <mujaheedpresdor001@gmail.com>
2026-03-01 11:33:35 +0100

    Add Flask app with home route and welcome message

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Welcome to My Platform</h1>
+    <p>This platform was created by Muhammad Mujahid 🚀</p>
+    """
+
+if __name__ == "__main__":
+    app.run()
